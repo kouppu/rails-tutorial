e19c3cce66c01962fce531c380dffc35d332f554
kou <suhrr.code@gmail.com>
2021-06-03 23:08:51 +0900

    Add follow and unfollow

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
     class_name: 'Relationship',
     foreign_key: "follower_id",
     dependent: :destroy
+  has_many :passive_relationships,
+    class_name: 'Relationship',
+    foreign_key: "followed_id",
+    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
@@ -83,6 +89,21 @@ class User < ApplicationRecord
 
   def feed()
     return Micropost.where('user_id = ?', id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    self.following() << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    self.active_relationships.find_by(followed_id: other_user.id).destroy()
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    self.following.include?(other_user)
   end
 
   # Static
